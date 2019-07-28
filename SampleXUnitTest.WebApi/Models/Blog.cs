7429884698aea5dc7fa17062eb650998016f9079
CaveDweller92 <harith.kotelawala@gmail.com>
2019-07-28 09:54:41 +0530

    Initial Commit

---
+++ SampleXUnitTest.WebApi/Models/Blog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleXUnitTest.WebApi.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+}
