b556ea12aced58766babc7bd90a13f892629ce75
CaveDweller92 <harith.kotelawala@gmail.com>
2019-07-29 05:41:52 +0530

    Add API Controllers

--- SampleXUnitTest.WebApi/BloggingContext.cs
+++ SampleXUnitTest.WebApi/BloggingContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SampleXUnitTest.WebApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,25 +13,14 @@ namespace SampleXUnitTest.WebApi
             : base(options)
         { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.Blog)
+                .WithMany(b => b.Posts);
+        }
+
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
-    }
-
-    public class Blog
-    {
-        public int BlogId { get; set; }
-        public string Url { get; set; }
-
-        public List<Post> Posts { get; set; }
-    }
-
-    public class Post
-    {
-        public int PostId { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-
-        public int BlogId { get; set; }
-        public Blog Blog { get; set; }
-    }
+    } 
 }
--- SampleXUnitTest.WebApi/Models/Post.cs
+++ SampleXUnitTest.WebApi/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,7 +12,9 @@ namespace SampleXUnitTest.WebApi.Models
         public string Title { get; set; }
         public string Content { get; set; }
 
-        public int BlogId { get; set; }
+        public int BlogForeignKey { get; set; }
+
+        [ForeignKey("BlogForeignKey")]
         public Blog Blog { get; set; }
     }
 }
